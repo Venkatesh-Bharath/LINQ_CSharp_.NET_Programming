f89767a861cbf1d3e59962f14553c701971536e8
VENKATESH BHARATH <152179142+Venkatesh-Bharath@users.noreply.github.com>
2024-08-09 17:44:44 +0530

    Update LinQDay_1.cs

--- LinQDay_1.cs
+++ LinQDay_1.cs
@@ -6,8 +6,35 @@ public class LinQDay_1{
   
     public static void Main(String[] args)
     {
-      
+     List<int> list=new List<int>{1,2,3,4,5,6,7,8,9};
+    // List<int> ans=new List<int>();
+     
+    // foreach(int e in list){
+    //   if(e%2!=0){
+    //     ans.Add(e);
+    //   }
+       
+    // }
+    
+    List<int> ans=list.Where(e=>e%2!=0).ToList();
+    Console.WriteLine(string.Join(" ",ans));
+    
+    
+    int a=10;
+    int b=30;
+    // max of 2 number
+    int max=new []{a,b}.Max();
+    Console.WriteLine(max);
+    // min of 2 number
+    int min=new []{a,b}.Min();
+    Console.WriteLine(min);
+    int c=45;
+    // max of 3 number
+    int max1=new []{a,b,c}.Max();
+    Console.WriteLine(max1);
 
     }
 
 }
+
+
